18f45ee19045c982a11d3b5bbbd38ff1b6f5a4ae
Catalin Pirvulescu <igunereve@gmail.com>
2023-08-29 00:42:49 +0300

    updates

--- test.js
+++ test.js
@@ -5,7 +5,14 @@ document.addEventListener("DOMContentLoaded", function () {
 
   switchButton.addEventListener("click", () => {
     document.querySelector("body").classList.toggle("white-mode");
-    document.querySelector(".cards").classList.toggle("white-cards");
+    document.querySelector(".head-title").classList.toggle("white-title");
+    document.querySelector(".mode").classList.toggle("white-title");
+    document.querySelector(".head").classList.toggle("white-border");
+
+    const cards = document.getElementsByClassName("cards");
+    for (const card of cards) {
+      card.classList.toggle("white-cards");
+    }
   });
 });
 
@@ -15,13 +22,13 @@ function setTitleHtml() {
   titleHtml += `
   <div class="header-title">
     <div class="head">
-      <h2>Social Media Dashboard</h2>
+      <h2 class="head-title">Social Media Dashboard</h2>
       <p class="subtitle">Total Followers: 23,004</p>
         </div>
         <div class="switch-mode">
           <p class="mode">Dark Mode</p>
           <label class="switch">
-          <input  type="checkbox" checked>
+          <input  type="checkbox">
           <span id="switch-btn" class="slider round"></span>
           </label>
         </div>
@@ -38,12 +45,14 @@ function setFeedHtml() {
     mediaHTML += `
     
 
-    <div class="cards white-cards">
+    <div class="cards ${socialData.borderClass}">
     <div class="flex-group">
-      <img src="images/icon-facebook.svg" />
-        <p class="social-name">
-        ${socialData.name}
+        <div class="social-tag">
+        <div class="${socialData.icon}"> 
+        <p class="social-name">${socialData.name}
         </p>
+        </div>
+        </div>
     </div>
 
       <h1 class="social-followers">
@@ -55,16 +64,17 @@ function setFeedHtml() {
       </p>
 
       <div class="statistics">
-      <img class="icon-up" 
-      src="images/icon-up.svg" />
-      <p class="social-data">
+      <div class="icons ${socialData.followersChange}
+        ${socialData.downArrow} ${socialData.upArrow}">
+        </div>
+        <p class="social-data ${socialData.downClass}">
         ${socialData.followersChange}
         </p>
       </div>
     </div>
-
     `;
   });
+
   return mediaHTML;
 }
 
